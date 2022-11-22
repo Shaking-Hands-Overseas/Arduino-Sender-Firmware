f9e74365dc5c748c1e2df5cb064cf955ee4f941d
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-11-22 09:17:21 +0100

    Update Sender.ino

--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -1,13 +1,15 @@
-int A[] = {36, 39, 32, 34, 35};
+int A[] = {A0, A1, A2, A3, A4};
 int maxes[] = {0, 0, 0, 0, 0};
 int mins[] = {180,180,180,180,180};
 
 void setup() {
   Serial.begin(115200); // // Serial Communication is starting with 115200 of baudrate speed
+
 }
+
 String reverse(int x){
     return String(map(x, 0, 4095, 180, 0)); // Reversing the values if needed
-  };
+};
 
 String normal(int s){
   return String(map(s, 0, 4095, 0, 180)); // Converting voltage to degrees 
@@ -25,13 +27,12 @@ void loop() {
     s[i] = map(s[i], mins[i], maxes[i], 0, 4095); 
   }
   
-  String sensors_reading[] = {reverse(s[0]), normal(s[1]), normal(s[2]), reverse(s[3]), normal(s[4])}; // Creating a dictionary where we have stored each value, 
+  String sensors_reading[] = {reverse(s[0]), normal(s[1]), normal(s[2]), normal(s[3]), normal(s[4])}; // Creating a dictionary where we have stored each value, 
                                                                                         // with reversed values included
   //Data Transmission to the python code via serial port
   char Read[100];
   Serial.readBytesUntil('A', Read, 100);
-  
-  String Info = "  " + sensors_reading[0] + "  " + sensors_reading[1] + "  " +  sensors_reading[2] + "  " + sensors_reading[3] + "  " + sensors_reading[4] + "  ";
+  String Info = "  " + String(sensors_reading[0]) + "  " + String(sensors_reading[1]) + "  " +  String(sensors_reading[2]) + "  " + String(sensors_reading[3]) + "  " + String(sensors_reading[4]) + "  ";
   Serial.println(Info); //We Send the data
   
   delay(150); // 1s of delay, to avoid overflow
